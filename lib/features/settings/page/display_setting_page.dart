7dfae40121d63769e2de5b35f489e44746cc21ed
amandwivedi07 <diligentdwivedi@gmail.com>
2025-07-09 21:33:55 +0530

    setting_screen

--- lib/features/settings/page/display_setting_page.dart
+++ lib/features/settings/page/display_setting_page.dart
@@ -2,9 +2,6 @@ import 'package:ausa/constants/typography.dart';
 import 'package:ausa/features/settings/controller/setting_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-import 'package:screen_brightness/screen_brightness.dart';
 
 class DisplaySettingPage extends StatefulWidget {
   const DisplaySettingPage({super.key});
